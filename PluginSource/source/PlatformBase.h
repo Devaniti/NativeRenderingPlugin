4338cc9b29491fd7a7dc5340d08ac28ec6bca1d5
Dmytro Bulatov <dmytro.bulatov@boolka.dev>
2023-05-22 12:36:19 +0300

    Changed sample to do buffer copy

--- PluginSource/source/PlatformBase.h
+++ PluginSource/source/PlatformBase.h
@@ -26,13 +26,6 @@
 	#else
 		#define UNITY_OSX 1
 	#endif
-#elif defined(__ANDROID__)
-	#define UNITY_ANDROID 1
-#elif defined(UNITY_METRO) || defined(UNITY_LINUX) || defined(UNITY_WEBGL) || defined (UNITY_EMBEDDED_LINUX) || defined (UNITY_EMBEDDED_LINUX_GL)
-	// these are defined externally
-#elif defined(__EMSCRIPTEN__)
-	// this is already defined in Unity 5.6
-	#define UNITY_WEBGL 1
 #else
 	#error "Unknown platform!"
 #endif
@@ -40,41 +33,13 @@
 
 
 // Which graphics device APIs we possibly support?
-#if UNITY_METRO
+#if UNITY_WIN
 	#define SUPPORT_D3D11 1
-	#if WINDOWS_UWP
-		#define SUPPORT_D3D12 1
-	#endif
-#elif UNITY_WIN
-	#define SUPPORT_D3D11 1 // comment this out if you don't have D3D11 header/library files
-	#define SUPPORT_D3D12 1 // comment this out if you don't have D3D12 header/library files
-	#define SUPPORT_OPENGL_UNIFIED 1
-	#define SUPPORT_OPENGL_CORE 1
-	#define SUPPORT_VULKAN 0 // Requires Vulkan SDK to be installed
-#elif UNITY_IOS || UNITY_TVOS || UNITY_ANDROID || UNITY_WEBGL
-	#ifndef SUPPORT_OPENGL_ES
-		#define SUPPORT_OPENGL_ES 1
-	#endif
-	#define SUPPORT_OPENGL_UNIFIED SUPPORT_OPENGL_ES
-	#ifndef SUPPORT_VULKAN
-		#define SUPPORT_VULKAN 0
-	#endif
-#elif UNITY_OSX || UNITY_LINUX
-	#define SUPPORT_OPENGL_UNIFIED 1
-	#define SUPPORT_OPENGL_CORE 1
-#elif UNITY_EMBEDDED_LINUX
-	#define SUPPORT_OPENGL_UNIFIED 1
-	#define SUPPORT_OPENGL_ES 1
-	#ifndef SUPPORT_VULKAN
-		#define SUPPORT_VULKAN 0
-	#endif
-#endif
-
-#if UNITY_IOS || UNITY_TVOS || UNITY_OSX
+#elif UNITY_IOS || UNITY_TVOS || UNITY_OSX
 	#define SUPPORT_METAL 1
+#else
+	#error Unknown platform
 #endif
-
-
 
 // COM-like Release macro
 #ifndef SAFE_RELEASE
